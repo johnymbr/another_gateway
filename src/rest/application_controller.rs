7a3567114724d4356cb470b891f1a443e42fa7b5
Johny Marques <johnymbr@gmail.com>
2022-12-08 15:14:34 -0300

    '#1 add find_application router.'

--- src/rest/application_controller.rs
+++ src/rest/application_controller.rs
@@ -1,13 +1,21 @@
-use axum::{routing::get, Router};
+use std::sync::Arc;
+
+use axum::{routing::get, Router, extract::{Query, State}, response::IntoResponse, Json};
+use hyper::StatusCode;
+use sqlx::PgPool;
+
+use crate::{model::Pagination, service::ApplicationService, exception::ApiError};
 
 pub struct ApplicationController;
 
 impl ApplicationController {
-    pub fn routes() -> Router {
+    pub fn routes(pg_pool: Arc<PgPool>) -> Router {
         Router::new().route("/application", get(ApplicationController::find_application))
+            .with_state(pg_pool)
     }
 
-    async fn find_application() -> &'static str {
-        "Hello, world!"
+    async fn find_application(Query(pagination): Query<Pagination>, State(pg_pool): State<Arc<PgPool>>) -> Result<impl IntoResponse, ApiError> {
+        let response = ApplicationService::find_application(pagination, &pg_pool).await?;
+        Ok((StatusCode::OK, Json(response)))
     }
 }
