12c2fef16139829834b02da855efb3ede07ac938
Johny Marques <johnymbr@gmail.com>
2022-12-23 15:25:38 -0300

    '#2 I included forward controller and in the next step I will set handle method in a service to the root route.'

--- src/service/forward_service.rs
+++ src/service/forward_service.rs
@@ -5,10 +5,11 @@ use axum::{
     http::{Request, Response},
 };
 use hyper::Body;
+use sqlx::PgPool;
 
 use crate::exception::{ApiError, APP_ERR_ID_IS_REQUIRED};
 
-use super::ApplicationServiceTrait;
+use super::{ApplicationServiceTrait, ApplicationService};
 
 #[async_trait]
 pub trait ForwardServiceTrait {
@@ -19,10 +20,26 @@ pub struct ForwardService {
     application_service: Arc<dyn ApplicationServiceTrait + Send + Sync>,
 }
 
+impl ForwardService {
+    pub fn new(pg_pool: Arc<PgPool>) -> Self {
+        ForwardService {
+            application_service: Arc::new(ApplicationService::new(Arc::clone(&pg_pool))),
+        }
+    }
+}
+
 #[async_trait]
 impl ForwardServiceTrait for ForwardService {
     async fn handle(&self, mut req: Request<Body>) ->  Result<Response<Body>, ApiError> {
         if let Some(applicationId) =req.headers().get("X-Application-Id") {
+            let path = req.uri().path();
+            tracing::info!("{}", path);
+
+            let path_segment: Vec<&str> = path.split("/").collect();
+            if !path_segment.is_empty() {
+                tracing::info!("{:?}", path_segment.first());
+            }
+
             Ok(Response::new(Body::empty()))
         } else {
             Err(ApiError::new(APP_ERR_ID_IS_REQUIRED))
