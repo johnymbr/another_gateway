ca23516cafac8b93aba61d6a479bc2b70cf16576
Johny Marques <johnymbr@gmail.com>
2022-12-28 15:40:19 -0300

    '#1 it was included routers to update an application and to delete an application.'

--- src/rest/application_controller.rs
+++ src/rest/application_controller.rs
@@ -23,14 +23,20 @@ impl ApplicationController {
     }
 
     pub fn routes(&self, pg_pool: Arc<PgPool>) -> Router {
-        let application_service: Arc<dyn ApplicationServiceTrait + Send + Sync> = Arc::new(ApplicationService::new(Arc::clone(&pg_pool)));
+        let application_service: Arc<dyn ApplicationServiceTrait + Send + Sync> =
+            Arc::new(ApplicationService::new(Arc::clone(&pg_pool)));
 
         Router::new()
             .route(
                 "/application",
                 get(ApplicationController::find_all).post(ApplicationController::save),
             )
-            .route("/application/:id", get(ApplicationController::find_by_id))
+            .route(
+                "/application/:id",
+                get(ApplicationController::find_by_id)
+                    .put(ApplicationController::update)
+                    .delete(ApplicationController::delete),
+            )
             .with_state(Arc::clone(&application_service))
     }
 
@@ -56,5 +62,22 @@ impl ApplicationController {
     ) -> Result<impl IntoResponse, ApiError> {
         let response = application_service.save(entity).await?;
         Ok((StatusCode::OK, Json(response)))
+    }
+
+    async fn update(
+        Path(id): Path<i64>,
+        State(application_service): State<Arc<dyn ApplicationServiceTrait + Send + Sync>>,
+        extract::Json(entity): extract::Json<ApplicationReq>,
+    ) -> Result<impl IntoResponse, ApiError> {
+        let response = application_service.update(id, entity).await?;
+        Ok((StatusCode::OK, Json(response)))
+    }
+
+    async fn delete(
+        Path(id): Path<i64>,
+        State(application_service): State<Arc<dyn ApplicationServiceTrait + Send + Sync>>,
+    ) -> Result<impl IntoResponse, ApiError> {
+        application_service.delete(id).await?;
+        Ok(StatusCode::NO_CONTENT)
     }
 }
