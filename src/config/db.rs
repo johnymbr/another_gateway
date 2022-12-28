961909733d56978dc343af98de1922a4a2097a0d
Johny Marques <johnymbr@gmail.com>
2022-12-28 15:26:52 -0300

    '#2 I finished the forward workflow. I needed to make some changes, like: adding hyper::Client as a field inside ForwardService struct. removing host header.'

--- src/config/db.rs
+++ src/config/db.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub struct Db;
 
@@ -11,7 +11,12 @@ impl Db {
 
         let pool = PgPoolOptions::new()
             .max_connections(10)
-            .acquire_timeout(Duration::from_secs(3))
+            .acquire_timeout(Duration::from_secs(
+                std::env::var("DATABASE_ACQUIRED_TIMEOUT")
+                    .unwrap_or(String::from("10"))
+                    .parse()
+                    .unwrap_or(10),
+            ))
             .connect(&db_connection_str)
             .await
             .expect("can connect to database");
--- src/service/forward_service.rs
+++ src/service/forward_service.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
+use std::{sync::Arc, collections::VecDeque, str::FromStr};
 
 use axum::{
     async_trait,
-    http::{Request, Response, uri::Uri},
+    http::{Request, Response, uri::Uri, HeaderValue},
 };
-use hyper::{Body, Client};
+use hyper::{Body, Client, header::HeaderName, Version, client::HttpConnector};
+use hyper_tls::HttpsConnector;
 use sqlx::PgPool;
 
 use crate::exception::{ApiError, FORWARD_ERR_PATH_IS_REQUIRED};
@@ -18,12 +19,15 @@ pub trait ForwardServiceTrait {
 
 pub struct ForwardService {
     application_service: Arc<dyn ApplicationServiceTrait + Send + Sync>,
+    client: Arc<Client<HttpsConnector<HttpConnector>>>,
 }
 
 impl ForwardService {
     pub fn new(pg_pool: Arc<PgPool>) -> Self {
+        let client = Client::builder().build(HttpsConnector::new());
         ForwardService {
             application_service: Arc::new(ApplicationService::new(Arc::clone(&pg_pool))),
+            client: Arc::new(client)
         }
     }
 }
@@ -34,20 +38,19 @@ impl ForwardServiceTrait for ForwardService {
         let path = req.uri().path();
         tracing::info!("{}", path);
 
-        let mut path_segment: Vec<&str> = path.split("/").collect();
-        path_segment = path_segment.into_iter().filter(|segment| !segment.trim().is_empty() || segment.eq(&"/")).collect::<Vec<&str>>();
+        let mut path_segment = path.split("/").collect::<VecDeque<&str>>();
+        path_segment = path_segment.into_iter().filter(|segment| !segment.trim().is_empty() || segment.eq(&"/")).collect::<VecDeque<&str>>();
         if path_segment.is_empty() {
             return Err(ApiError::new(FORWARD_ERR_PATH_IS_REQUIRED));
         }
 
-        if let Some(path_application) = path_segment.first() {
+        if let Some(path_application) = path_segment.pop_front() {
             tracing::info!("{:?}", path_application);
 
             let application = self.application_service.find_by_path((String::from("/") + path_application).as_str()).await?;
             tracing::info!("{:?}", application.path);
 
-            // TODO: create hyper Client, change uri of request and send new request.
-            let mut new_path_and_query = path_segment.join("/");
+            let mut new_path_and_query = Vec::from(path_segment).join("/");
             if let Some(query) = req.uri().query() {
                 new_path_and_query = new_path_and_query + query;
             }
@@ -55,10 +58,17 @@ impl ForwardServiceTrait for ForwardService {
             let new_uri = format!("{}/{}", application.url_destination, new_path_and_query);
 
             *req.uri_mut() = Uri::try_from(new_uri).unwrap();
+            req.headers_mut().remove(HeaderName::from_static("host"));
 
-            let client = Client::new();
+            tracing::info!("{:?}", req);
 
-            let response = client.request(req).await?;
+            let response = self.client.request(req).await.map_err(|e| {
+                tracing::error!("{:?}", e);
+                e
+            })?;
+
+            tracing::info!("{:?}", response);
+
             Ok(response)
         } else {
             Err(ApiError::new(FORWARD_ERR_PATH_IS_REQUIRED))
